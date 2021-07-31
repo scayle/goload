66133cc855c66c05a1f66e076854d367f37c81fc
HenriBeck <henribeck.dev@gmail.com>
2021-07-31 12:19:54 +0000

    feat(http): Extend HTTP connection pool options

--- http/connection_pool.go
+++ http/connection_pool.go
@@ -9,11 +9,43 @@ type HTTPClientPool struct {
 	clients []*http.Client
 }
 
-func NewHTTPConnectionPool(count int) *HTTPClientPool {
+type HTTPTransportOption = func(request *http.Request)
+
+func WithUserAgentOption(userAgent string) HTTPTransportOption {
+	return func(request *http.Request) {
+		if request.Header.Get("user-agent") == "" {
+			request.Header.Set("user-agent", userAgent)
+		}
+	}
+}
+
+type transport struct {
+	options []HTTPTransportOption
+
+	innerTransport http.RoundTripper
+}
+
+func (transport *transport) RoundTrip(request *http.Request) (*http.Response, error) {
+	for _, option := range transport.options {
+		option(request)
+	}
+
+	return transport.innerTransport.RoundTrip(request)
+}
+
+func NewHTTPConnectionPool(
+	count int,
+	options ...HTTPTransportOption,
+) *HTTPClientPool {
 	clients := make([]*http.Client, count)
 
 	for i := 0; i < count; i++ {
-		clients[i] = &http.Client{}
+		clients[i] = &http.Client{
+			Transport: &transport{
+				options:        options,
+				innerTransport: http.DefaultTransport,
+			},
+		}
 	}
 
 	return &HTTPClientPool{
